a54515d9c94ebbb6764b98846bde74d86dcf1564
jean maciel <Jeansud@hotmail.com>
2020-07-30 00:13:13 -0300

    JKFLIX COMMIT

---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroCaategoria() {
+    return (
+      <PageDefault>
+        <h1>Cadastro de Categoria</h1>
+
+        
+
+
+        <Link to="/">
+          Ir para Home
+        </Link>
+      </PageDefault>
+    )
+  }
+
+export default CadastroCaategoria;
