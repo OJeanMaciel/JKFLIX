c3e9cc427dd3bc862dc539695ce26815270f425a
jean maciel <Jeansud@hotmail.com>
2020-08-01 23:34:40 -0300

    Update Máximo!!!

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,8 +28,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    console.log('Alo, Brasil');
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://jkflix.herokuapp.com/categorias';
     fetch(URL)
     .then(async (respostaDoServidor) => {
       const resposta = await respostaDoServidor.json();
